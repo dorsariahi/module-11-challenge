22ec3c1ca54a29a9065117ceaffb027812d19930
dorsariahi <dorsariahiasl@gmail.com>
2023-01-03 18:31:10 -0600

    added most of the router

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,20 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+let dataBase = require('../db/db.json')
+
 router.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, './db/db.json'))
+    res.sendFile(path.join(__dirname, '../db/db.json'))
 );
+
+router.post('/notes', (req, res) =>{
+    //before we push req.body we need to add an id to the note UU id
+dataBase.push(req.body);
+fs.writeFile('./db/db.json', JSON.stringify(dataBase), function (err) {
+    if (err) throw err;
+    console.log('succsesfully saved to the db File');
+  });
+  res.json(req.body)}
+);
+
+module.exports = router
