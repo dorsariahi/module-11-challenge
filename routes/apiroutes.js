1f7d7374b5b109f4c803f1943979b0f3593c4399
dorsariahi <dorsariahiasl@gmail.com>
2023-01-09 12:52:22 -0600

    push the new changes( finsal push)

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,19 +2,32 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 let dataBase = require('../db/db.json')
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '../db/db.json'))
 );
 
-router.post('/notes', (req, res) =>{
+router.post('/notes', (req, res) => {
     //before we push req.body we need to add an id to the note UU id
-dataBase.push(req.body);
-fs.writeFile('./db/db.json', JSON.stringify(dataBase), function (err) {
-    if (err) throw err;
-    console.log('succsesfully saved to the db File');
-  });
-  res.json(req.body)}
+    console.log(uuidv4())
+    req.body.id = uuidv4()
+    dataBase.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(dataBase), function (err) {
+        if (err) throw err;
+        console.log('succsesfully saved to the db File');
+    });
+    res.json(req.body)
+}
 );
 
+router.delete('/notes/:id', (req, res) => {
+    let noteId = req.params.id
+    dataBase = dataBase.filter(note => note.id !== noteId)
+    fs.writeFile('./db/db.json', JSON.stringify(dataBase), function (err) {
+        if (err) throw err;
+        console.log('succsesfully deleted');
+    });
+    res.sendStatus(200)
+})
 module.exports = router
